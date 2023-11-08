225ba6886454996bfb0bed1f26a40b8cdb68eca2
HungTran <32216529+hungtranvn@users.noreply.github.com>
2023-11-08 04:48:02 +0000

    graph examples

---
+++ HomeWork/BFS/EdgeExistance.cpp
@@ -0,0 +1,50 @@
+/*
+You have been given an undirected graph consisting of N nodes and M edges. 
+This graph can consist of self-loops as well as multiple edges. 
+In addition , you have also been given Q queries. 
+For each query , you shall be given 2 integers A and B. 
+You just need to find if there exists an edge between node A and node B. 
+If yes, print "YES" (without quotes) else , print "NO"(without quotes).
+
+Input Format:
+The first line consist of 2 integers N and M denoting the number of nodes and edges respectively. 
+Each of the next M lines consist of 2 integers A and B denoting an undirected edge between node A and B. 
+The next line contains a single integer Q denoting the number of queries. 
+The next Line contains 2 integers A and B denoting the details of the query.
+Output Format
+Print Q lines, the answer to each query on a new line.
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+void do_exist () {
+
+}
+
+int main() {
+    int N, M;
+    cin>> N >> M;
+    vector<int> adj[N];
+
+    while (M > 0)
+    {
+        int A, B;
+        cin >> A >> B;
+        adj[A].push_back(B);
+        adj[B].push_back(A);
+        M--;
+    }
+
+    int Q;
+    cin>>Q;
+    while (Q > 0) {
+        int A, B;
+        cin >> A >> B;
+        Q--;
+    }
+    return 0;
+}
---
+++ HomeWork/BFS/LevelNode.cpp
@@ -0,0 +1,27 @@
+/*
+You have been given a Tree consisting of N nodes. A tree is a fully-connected graph consisting of N nodes and edges. 
+The nodes in this tree are indexed from 1 to N. Consider node indexed 1 to be the root node of this tree.
+The root node lies at level one in the tree.
+You shall be given the tree and a single integer x.
+You need to find out the number of nodes lying on level x.
+
+Input Format
+The first line consists of a single integer N denoting the number of nodes in the tree.
+Each of the next lines consists of 2 integers a and b denoting an undirected edge between node a and node b.
+The next line consists of a single integer x.
+
+Output Format
+You need to print a single integers denoting the number of nodes on level x.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    unordered_map<int, vector<int>> vertices;
+    return 0;
+}
