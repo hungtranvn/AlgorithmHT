72593b40f7e2e0215081db4280db9fd099eb8795
HungTran <32216529+hungtranvn@users.noreply.github.com>
2024-05-23 03:51:21 +0000

    merged sort OK

--- docs/source/mit_book/codes/1.foundation/foundation.py
+++ docs/source/mit_book/codes/1.foundation/foundation.py
@@ -1,3 +1,5 @@
+import sys
+
 def insertion_sort(A):
     for j in range(2, len(A)):
         key = A[j]
@@ -16,8 +18,8 @@ def merge_sort(A, a, b = None):
         c = (a + b) // 2
         merge_sort(A, a, c) # call log(n) times
         merge_sort(A, c, b)
-        
-        #merge(A, i, k, j)
+        merge_sorted_arrays(A, a, c, b)
+        '''
         L, R = A[a:c], A[c:b]
         i , j = 0, 0
         while a < b: # O(n) times
@@ -27,11 +29,65 @@ def merge_sort(A, a, b = None):
             else:
                 A[a] = R[j]
                 j = j + 1
-            a = a + 1 
+            a = a + 1
+        '''
     return A
 
+def merge_sorted_arrays(A, a, c, b):
+    print("Index:", a, " ", c, " ", b)
+
+    # make two new arrays for comparison
+    L, R = A[a:c], A[c:b]
+    
+    # merge two sorted ranges in one
+    i, j = 0, 0
+    count = a
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            A[count] = L[i]
+            i = i + 1
+        else:
+            A[count] = R[j]
+            j = j + 1
+        count = count + 1
+    while i < len(L):
+        A[count] = L[i]
+        i = i + 1
+        count = count + 1
+    while j < len(R):
+        A[count] = R[j]
+        j = j + 1
+        count = count + 1
+        
+    print("A_sorted = ", A)
+
+'''
+def merge_sorted_arrays(A, p, q, r):
+    #print("Index:", p, " ", q, " ", r)
+    n1 = len(A[p:q])
+    n2 = len(A[q:r])
+
+    # make two new arrays for comparison
+    L, R = [None]*(n1), [None]*(n2)
+    for i in range(0, n1, 1):
+        L[i] = A[p + i]
+    for j in range(0, n2, 1):
+        R[j] = A[q + j]
+    
+    # merge two sorted ranges in one
+    i, j = 0, 0
+    for k in range(p, r, 1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i = i + 1
+        else:
+            A[k] = R[j]
+            j = j + 1
+    #print("A_sorted = ", A)
+'''
 def main():
     A = [1,4,2,7,6,5]
+    print("A = ", A)
     print(merge_sort(A, 0, len(A)))
     #print(A)
 
